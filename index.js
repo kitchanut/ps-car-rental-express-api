f34a3d0ad806f635360976a6a73c43e449a7ec4a
kitchanut <kitchanut.r@gmail.com>
2024-08-28 21:48:36 +0700

    sendLineNotify

--- index.js
+++ index.js
@@ -16,11 +16,11 @@ app.get("/", async (req, res) => {
     message: "Welcome to the PS-Car-Rental REST API",
   });
 });
-const crypto = require("crypto");
-app.get("/generatetoken", async (req, res) => {
-  const VERIFY_TOKEN = crypto.randomBytes(32).toString("hex");
-  console.log("Your VERIFY_TOKEN:", VERIFY_TOKEN);
-});
+// const crypto = require("crypto");
+// app.get("/generatetoken", async (req, res) => {
+//   const VERIFY_TOKEN = crypto.randomBytes(32).toString("hex");
+//   console.log("Your VERIFY_TOKEN:", VERIFY_TOKEN);
+// });
 
 app.get("/webhook", (req, res) => {
   // รับข้อมูลจาก query string
@@ -43,19 +43,21 @@ app.get("/webhook", (req, res) => {
 
 app.post("/webhook", (req, res) => {
   const body = req.body;
-
   if (body.object === "page") {
     body.entry.forEach(async (entry) => {
       const webhookEvent = entry.messaging[0];
       console.log(webhookEvent);
       try {
-        const facebook_messages = await prisma.facebook_messages.create({
-          data: {
-            sender_id: webhookEvent.sender.id,
-            recipient_id: webhookEvent.recipient.id,
-            message: webhookEvent.message.text,
-          },
-        });
+        // Object to String
+        const webhookEventString = JSON.stringify(webhookEvent);
+        sendLineNotify(webhookEventString);
+        // const facebook_messages = await prisma.facebook_messages.create({
+        //   data: {
+        //     sender_id: webhookEvent.sender.id,
+        //     recipient_id: webhookEvent.recipient.id,
+        //     message: webhookEvent.message.text,
+        //   },
+        // });
         // res.status(200).json(facebook_messages);
       } catch (error) {
         res.status(500).json({ error: "An error occurred while creating the branches." });
@@ -69,6 +71,15 @@ app.post("/webhook", (req, res) => {
     res.sendStatus(404);
   }
 });
+const LINE_NOTIFY_TOKEN = "duFnBTlRddOVE4ywC3l7uYg6QxK8naXVg2GhAWYG0Nu";
+const sendLineNotify = (message) => {
+  return axios.post("https://notify-api.line.me/api/notify", `message=${encodeURIComponent(message)}`, {
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      Authorization: `Bearer ${LINE_NOTIFY_TOKEN}`,
+    },
+  });
+};
 
 const routes = require("./routes");
 app.use("/api", routes);
